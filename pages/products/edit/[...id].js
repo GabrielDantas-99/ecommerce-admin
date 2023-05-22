3849dadabd5be1729762d5f736337cbb5d261730
Gabriel Dantas <gabrieldantaslp@gmail.com>
2023-05-22 10:04:44 -0300

    Push to remote

--- pages/products/edit/[...id].js
+++ pages/products/edit/[...id].js
@@ -2,8 +2,10 @@ import Layout from "@/components/Layout";
 import {useRouter} from "next/router";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import ProductForm from '@/components/ProductForm';
 
 export default function EditProductPage() {
+    
   const [productInfo, setProductInfo] = useState(null);
   const router = useRouter();
   const {id} = router.query;
@@ -12,13 +14,13 @@ export default function EditProductPage() {
       return;
     }
     axios.get('/api/products?id='+id).then(response => {
-        console.log(response.data)
+        console.log(response)
       setProductInfo(response.data);
     });
   }, [id]);
   return (
     <Layout>
-      <h1>Edit product</h1>
+      <ProductForm />
     </Layout>
   );
 }
--- pages/products/new.js
+++ pages/products/new.js
@@ -1,58 +1,10 @@
-import Layout from '@/components/Layout'
-import axios from 'axios';
-import { useState } from 'react';
-import { redirect } from 'next/dist/server/api-utils';
-import { useRouter } from 'next/router';
+import Layout from '@/components/Layout';
+import ProductForm from '@/components/ProductForm';
 
 export default function NewProduct() {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [price, setPrice] = useState('');
-    const [goToProducts, setGoToProducts] = useState(false);
-    const router = useRouter();
-
-    async function createProduct(ev) {
-        ev.preventDefault();
-        const data = {title, description, price};
-        await axios.post('/api/products', data);
-        setGoToProducts(true);
-    }
-
-    if (goToProducts) {
-        router.push('/products');
-    }
-
     return (
         <Layout>
-            <form onSubmit={createProduct}>
-                <h1>New Product</h1>
-                <label>Product name</label>
-                <input 
-                    value={title}
-                    onChange={ev => setTitle(ev.target.value)}
-                    type="text" 
-                    placeholder='Product name' 
-                    />
-                <label>Description</label>
-                <textarea 
-                    value={description}
-                    onChange={ev => setDescription(ev.target.value)}
-                    placeholder='Description'>
-                </textarea> 
-                <label>Price (in USD)</label>
-                <input 
-                    value={price}
-                    onChange={ev => setPrice(ev.target.value)}
-                    type="number" 
-                    placeholder='Price' 
-                    />
-                <button 
-                    type='submit'
-                    className='btn-primary'
-                    >
-                    Save
-                </button>
-                </form>
+            <ProductForm />
         </Layout>
-    )
+    ) 
 }
